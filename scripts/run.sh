5b0a97c8ed676d0ed728794fbcc95a13647caca2
Alex Lau <alau@windows98.local>
2019-08-23 01:19:14 +0900

    cleanup run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-sock_path="$HOME/.epc.sock"
-log_path="/dev/null"
+# emacs-pool-daemon's unix domain socket path
+sock_path="$HOME/.emacs-pool.sock"
+
+# emacs-pool-daemon's log file path
+log_file_path="/dev/null"
+
+# emacs bin directory containing emacs and emacsclient.
 emacs_bin_path="$HOME/emacs-build/bin"
 
 if [[ "${OSTYPE}" == "darwin"* ]]; then
@@ -11,10 +16,16 @@ else
 fi
 
 if [ -z "${is_running}" ]; then
-    echo "Starting daemon."
+    echo "Starting emacs-pool-daemon.."
     rm -f "${sock_path}"
-    RUST_LOG=info nohup emacs-pool-daemon --sock "${sock_path}" --emacs "${emacs_bin_path}/emacs" &>"${log_path}" &
-    sleep 0.5
+    RUST_LOG=info nohup emacs-pool-daemon \
+            --sock "${sock_path}" \
+            --emacs "${emacs_bin_path}/emacs" \
+        &>"${log_file_path}" &
+    sleep 1
 fi
 
-exec emacs-pool-client --sock "${sock_path}" --emacsclient "${emacs_bin_path}/emacsclient" $@
+exec emacs-pool-client \
+     --sock "${sock_path}" \
+     --emacsclient "${emacs_bin_path}/emacsclient" \
+     "$@"
